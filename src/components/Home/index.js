ce64152c6d4ee308d1d960dbd13aa716c4c744a9
vishnuSrishti <vishnusekhar.sics@gmail.com>
2022-07-26 19:48:30 +0530

    initial commit

---
+++ src/components/Home/index.js
@@ -0,0 +1,12 @@
+import './index.scss'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='container home-page'>
+        Home
+    </div>
+  )
+}
+
+export default Home
